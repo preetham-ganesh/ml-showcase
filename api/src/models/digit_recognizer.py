5eb6aa463aa1f6d23af753585fedea64c9e316c5
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-01-27 17:13:09 -0500

    [#2] added predict_digit() to DigitRecognizer class.
    Preprocesses image based on model requirements. Predicts digit recognized from image.

--- api/src/models/digit_recognizer.py
+++ api/src/models/digit_recognizer.py
@@ -6,6 +6,8 @@ import numpy as np
 
 from src.utils import load_json_file
 
+from typing import Dict, Any
+
 
 class DigitRecognizer(object):
     """Recognizes digit in an image."""
@@ -109,6 +111,10 @@ class DigitRecognizer(object):
         # Asserts type & value of the arguments.
         assert isinstance(image, np.ndarray), "Variable image of type 'np.ndarray'."
 
+        # If image is in RGB format, converts it to grayscale.
+        if len(image.shape) == 2:
+            image = np.dot(image[..., :3], [0.2989, 0.5870, 0.1140])
+
         # Expands the dimensions of the image in the first axis.
         image = np.expand_dims(image, axis=0)
 
@@ -116,3 +122,50 @@ class DigitRecognizer(object):
         image = np.float32(image)
         image = image / 255.0
         return image
+
+    def predict_digit(self, image: np.ndarray) -> Dict[str, Any]:
+        """Preprocesses image based on model requirements. Predicts digit recognized from image.
+
+        Preprocesses image based on model requirements. Predicts digit recognized from image.
+
+        Args:
+            image: A NumPy array for the image.
+
+        Returns:
+            A dictionary for status of the prediction, along with predicted digit & prediction's confidence score.
+        """
+        # Asserts type & value of the arguments.
+        assert isinstance(
+            image, np.ndarray
+        ), "Variable image should be of type 'np.ndarray'."
+
+        # Preprocesses image based on model requirements.
+        model_input_image = self.preprocess_image(image)
+
+        # Sends model input image as input to Model using URL.
+        try:
+            response = requests.post(
+                self.model_api_url,
+                data=json.dumps({"inputs": model_input_image.tolist()}),
+                headers={"content-type": "application/json"},
+            )
+        except requests.exceptions.ConnectionError:
+            return {
+                "status": "Failure",
+                "message": "Serving URL does not exist. Received 'requests.exceptions.ConnectionError' error.",
+            }
+
+        # If status is 200, then extracts the prediction from the response.
+        if response.status_code == 200:
+            prediction = np.array(
+                json.loads(response.text)["outputs"], dtype=np.float32
+            )
+
+            # Computes the digit predicted by the model, & extracts the confidence score.
+            predicted_digit = int(np.argmax(prediction[0]))
+            score = float(prediction[0][predicted_digit])
+            return {"status": "Success", "digit": predicted_digit, "score": score}
+
+        # Else returns the text from response.
+        else:
+            return {"status": "Failure", "message": response.text}
