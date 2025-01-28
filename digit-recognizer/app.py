13bd06296544ab0688df22ed051c258ab3af90ae
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-01-28 12:15:37 -0500

    [#4] added error() to digit_recognizer app.
    Renders template for error message.

--- digit-recognizer/app.py
+++ digit-recognizer/app.py
@@ -1,6 +1,6 @@
 import uuid
 import os
-import time
+import argparse
 
 from flask import Flask, send_from_directory, request, render_template, redirect
 import requests
@@ -13,7 +13,7 @@ import skimage
 app = Flask(__name__)
 
 
-@app.route("/send_image/")
+@app.route("/send_image")
 def send_image():
     """Sends a saved image from the directory based on the provided file path.
 
@@ -34,3 +34,83 @@ def send_image():
         return send_from_directory(directory, file_name)
     except Exception as e:
         return f"Error: {str(e)}", 400
+
+
+@app.route("/error")
+def error(submission_id: str, message: str) -> str:
+    """Renders template for error message.
+
+    Renders template for error message.
+
+    Args:
+        submission_id: A string for the unique id of the submission.
+        message: A string for the error message.
+
+    Returns:
+        A string for the rendered template for error.
+    """
+    return render_template("error.html", submission_id=submission_id, message=message)
+
+
+@app.route("/in_progress", methods=["GET", "POST"])
+def in_progress(image_file_path: str, submission_id: str) -> str:
+    """"""
+    if request.method == "POST":
+        _ = ""
+    else:
+        return render_template(
+            "in_progress.html",
+            input_file_path=image_file_path,
+            submission_id=submission_id,
+        )
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload() -> str:
+    """Renders template for uploading image to Digit Recognizer. Recognizes digit in image.
+
+    Renders template for uploading image to Digit Recognizer. Recognizes digit in image.
+
+    Args:
+        None.
+
+    Returns:
+        A string for the rendered template for upload or complete.
+    """
+    # Checks if request method is POST, then predicts digit for uploaded image.
+    if request.method == "POST":
+        # Extracts image file path from request.
+        image_file_path = request.form["selected_image"]
+        image_file_path = image_file_path.lstrip("../")
+
+        # Generates a unique id for this submission to ensure traceability.
+        submission_id = str(uuid.uuid4())
+
+        # Defines the API URL for submitting the image to the ML model.
+        submit_image_api_url = f"{host_url}/api/v1/submit_image"
+
+        #
+
+
+if __name__ == "__main__":
+    print()
+
+    # Parses the arguments.
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-dt",
+        "--deployment_type",
+        type=str,
+        required=True,
+        help="Type of the deployment.",
+    )
+    args = parser.parse_args()
+
+    # Based on the type of deployment, the host URL is set.
+    if args.deployment_type == "local":
+        host_url = "http://localhost:8100"
+    else:
+        host_url = "http://host.docker.internal:8100"
+
+    # Runs app on specified host & port (For local deployment)
+    app.run(host="0.0.0.0", port=3000)
