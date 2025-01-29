b972e602e8efac23da3758580371f40e4724e520
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-01-28 20:54:32 -0500

    [#7] added load_model_configuration() to FlairAbnormalityClassification class.
    Loads the model configuration file for model version.

--- api/src/models/bms_flair_abnormality_classification.py
+++ api/src/models/bms_flair_abnormality_classification.py
@@ -1,3 +1,8 @@
+import os
+
+from src.utils import load_json_file
+
+
 class FlairAbnormalityClassification(object):
     """Predicts whether is FLAIR abnormality in brain MRI images."""
 
@@ -21,3 +26,25 @@ class FlairAbnormalityClassification(object):
         self.model_version = model_version
         self.model_api_url = model_api_url
         self.id_to_class = {0: "no_abnormality", 1: "abnormality"}
+
+    def load_model_configuration(self) -> None:
+        """Loads the model configuration file for model version.
+
+        Loads the model configuration file for model version.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.home_directory_path = os.getcwd()
+        model_configuration_directory_path = os.path.join(
+            self.home_directory_path,
+            "configs",
+            "models",
+            "bms_flair_abnormality_classification",
+        )
+        self.model_configuration = load_json_file(
+            f"v{self.model_version}", model_configuration_directory_path
+        )
